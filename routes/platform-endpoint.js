1b9db3acf03246f43ea955d45ed292812698f07d
MattxDamon <mbmattbarth@gmail.com>
2015-12-22 11:22:24 -0500

    better logs

--- routes/platform-endpoint.js
+++ routes/platform-endpoint.js
@@ -13,9 +13,11 @@ exports.createPlatformEndpoint = function(platform, token) {
 	var applicationArn = null;
 	if (platform === "apple") {
 		applicationArn = process.env.APPLE_ARN_ID;
+		console.log("Apple device registering");
 		// applicationArn = 'arn:aws:sns:us-east-1:085864041402:app/APNS/ODDPBRTestApp';
 	} else if (platform === "android") {
 		applicationArn = process.env.ANDROID_ARN_ID;
+		console.log("Android device registering");
 	}
 
 	return new Promise(function(resolve, reject) {
